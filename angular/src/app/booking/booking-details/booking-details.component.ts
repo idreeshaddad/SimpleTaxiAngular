c46e5d07eccc46e75bee6762f18d0048ef4602d2
Idrees Haddad <gelnox@live.com>
2021-12-05 17:10:09 +0000

    Added booking details page

--- angular/src/app/booking/booking-details/booking-details.component.ts
+++ angular/src/app/booking/booking-details/booking-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Booking } from 'src/app/shared/models/Booking';
+import { BookingService } from '../booking.service';
 
 @Component({
   selector: 'app-booking-details',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingDetailsComponent implements OnInit {
 
-  constructor() { }
+  booking!: Booking;
+
+  constructor(
+    private bookingSvc: BookingService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+
+    const bookingId: number = Number(this.route.snapshot.paramMap.get("id"));
+
+    this.bookingSvc.getBookingById(bookingId).subscribe(
+      res =>
+        this.booking = res
+    );
   }
 
 }
